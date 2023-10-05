08adab4be58658d00f49edd52815414ece0848f5
supakornnetsuwan <earth_killerdark@hotmail.com>
2023-10-05 16:27:36 +0700

    styles(application): Create basic app style

--- src/core/components/CustomDialog.tsx
+++ src/core/components/CustomDialog.tsx
@@ -1,10 +1,12 @@
 "use client";
+import { useState } from "react";
 import {
   Dialog,
   DialogContent,
   DialogDescription,
   DialogFooter,
   DialogOverlay,
+  DialogClose,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
@@ -13,22 +15,55 @@ import {
 type CustomDialogPropsType = {
   title: string;
   description: string;
-  trigger: React.ReactElement;
+  trigger: React.FC<{ open: () => void }>;
   body?: React.ReactElement;
-  footer?: React.ReactElement;
+  footer?: React.FC<{ close: () => void }>;
 };
 
-const CustomDialog: React.FC<CustomDialogPropsType> = ({ title, description, trigger, body, footer }) => {
+/**
+ * 
+ * @example
+ * ``` 
+    <CustomDialog
+      title="Welcome!"
+      description="This is our brand new website! please enjoy playing around here"
+      body={<div>A cool product description</div>}
+      trigger={({ open }) => <Button onClick={open}>Open</Button>}
+      footer={({ close }) => (
+        <div>
+          <Button onClick={close} variant="secondary">
+            <ThumbsUp size="20px" />
+            <span className="ml-2">Set wakeup time</span>
+          </Button>
+        </div>
+      )}
+    />
+    ```
+ */
+
+const CustomDialog: React.FC<CustomDialogPropsType> = ({
+  title,
+  description,
+  trigger: Trigger,
+  body,
+  footer: Footer,
+}) => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <Dialog>
-      <DialogTrigger asChild>{trigger}</DialogTrigger>
+    <Dialog open={open}>
+      <DialogTrigger asChild>
+        <Trigger open={() => setOpen(true)} />
+      </DialogTrigger>
+      <DialogOverlay className="bg-slate-500/20 backdrop-blur-sm" onClick={(e) => setOpen(false)} />
       <DialogContent className="bg-white">
+        <DialogClose onClick={() => setOpen(false)} />
         <DialogHeader>
           <DialogTitle>{title}</DialogTitle>
           <DialogDescription>{description}</DialogDescription>
         </DialogHeader>
         <div>{body}</div>
-        <DialogFooter>{footer}</DialogFooter>
+        <DialogFooter>{Footer && <Footer close={() => setOpen(false)} />}</DialogFooter>
       </DialogContent>
     </Dialog>
   );
