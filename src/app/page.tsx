492f8c42d74aa81f89969201383d77678b3122ee
supakornnetsuwan <earth_killerdark@hotmail.com>
2023-10-05 16:27:36 +0700

    Style : Create basic app style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
+"use client";
 import Wrapper from "@/core/layouts/Wrapper";
-import CustomDialog from "@/core/components/CustomDialog";
-import { Button } from "@/core/components/ui/button";
-import { AlarmClock } from "lucide-react";
 
 const Home = () => {
   return (
@@ -9,17 +7,6 @@ const Home = () => {
       <div>
         <h2 className="text-slate-800 font-bold text-lg">Hello my applciation</h2>
       </div>
-      <Button variant="secondary">
-        <AlarmClock />
-        <span className="ml-2">Set wakeup time</span>
-      </Button>
-      <CustomDialog
-        title="Welcome!"
-        description="Welcome to our brand new website! please enjoy playing around here"
-        body={<div>Product</div>}
-        trigger={<Button>Open</Button>}
-        footer={<div>footer</div>}
-      />
       <div className="text-slate-500">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio, ipsam.</div>
     </Wrapper.Base>
   );
--- src/core/components/ui/dialog.tsx
+++ src/core/components/ui/dialog.tsx
@@ -32,7 +32,6 @@ const DialogContent = React.forwardRef<
   React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
 >(({ className, children, ...props }, ref) => (
   <DialogPortal>
-    <DialogOverlay className="bg-slate-500/20 backdrop-blur-sm" />
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
@@ -42,14 +41,30 @@ const DialogContent = React.forwardRef<
       {...props}
     >
       {children}
-      <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-0 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground ">
-        <X className="h-4 w-4 " />
-        <span className="sr-only">Close</span>
-      </DialogPrimitive.Close>
     </DialogPrimitive.Content>
   </DialogPortal>
 ));
 DialogContent.displayName = DialogPrimitive.Content.displayName;
+
+const DialogClose = React.forwardRef<
+  React.ElementRef<typeof DialogPrimitive.Close>,
+  React.ComponentPropsWithoutRef<typeof DialogPrimitive.Close>
+>(({ className, ...props }, ref) => {
+  return (
+    <DialogPrimitive.Close
+      {...props}
+      className={cn(
+        "absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-0 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground",
+        className
+      )}
+    >
+      <X className="h-4 w-4 " />
+      <span className="sr-only">Close</span>
+    </DialogPrimitive.Close>
+  );
+});
+
+DialogClose.displayName = "DialogClose";
 
 const DialogHeader = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
   <div className={cn("flex flex-col space-y-1.5 text-center sm:text-left", className)} {...props} />
@@ -91,4 +106,5 @@ export {
   DialogFooter,
   DialogTitle,
   DialogDescription,
+  DialogClose,
 };
